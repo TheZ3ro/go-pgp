7bb18cb8222774025e8d844a3a0e11aef0431c63
thez3ro <io@thezero.org>
2017-12-15 16:22:01 +0100

    open keyring from file, load entity by email from entityList

--- pgp/key.go
+++ pgp/key.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/crypto/openpgp"
 	"golang.org/x/crypto/openpgp/armor"
 	"golang.org/x/crypto/openpgp/packet"
+	"os"
 )
 
 func getPublicKeyPacket(publicKey []byte) (*packet.PublicKey, error) {
@@ -53,4 +54,29 @@ func getPrivateKeyPacket(privateKey []byte) (*packet.PrivateKey, error) {
 		return nil, errors.New("Unable to cast to Private Key")
 	}
 	return key, nil
+}
+
+func OpenKeyring(keyringPath string) (openpgp.EntityList, error) {
+	keyringFileBuffer, err := os.Open(keyringPath)
+	if err != nil {
+		return nil, err
+	}
+	defer keyringFileBuffer.Close()
+	entityList, err := openpgp.ReadKeyRing(keyringFileBuffer)
+	if err != nil {
+		return nil, err
+	}
+	return entityList, nil
+}
+
+func GetKeyByEmail(keyring openpgp.EntityList, email string) *openpgp.Entity {
+	for _, entity := range keyring {
+		for _, ident := range entity.Identities {
+			if ident.UserId.Email == email {
+				return entity
+			}
+		}
+	}
+
+	return nil
 }
